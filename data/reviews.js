f9c9c52697039648c83949a86767f03cb07f6914
Patricia Muresan <patriciamariamrsn@gmail.com>
2024-01-11 18:43:55 +0200

    created the post method for the reviews

--- data/reviews.js
+++ data/reviews.js
@@ -1,18 +1,22 @@
 const reviews = [
     {
         id: 1,
+        name: "Mikey",
         review: "nice color"
     },
     {
         id: 2,
+        name: "Jenny",
         review: "nice product"
     },
     {
         id: 3,
+        name: "Paul",
         review: "great delivery service"
     },
     {
         id: 4,
+        name: "Kate",
         review: "not useful"
     },
 ];
--- routes/reviews.js
+++ routes/reviews.js
@@ -10,5 +10,16 @@ router
 .get((req, res) => {
     res.json(reviews)
 })
+.post((req, res) => {
+    if(req.body.name && req.body.review) {
+        const review = {
+            id: reviews[reviews.length -1].id + 1,
+            name: req.body.name,
+            review: req.body.review
+        };
+        reviews.push(review);
+        res.json(reviews[reviews.length - 1]);
+    } else { console.log("insufficient data")}
+})
 
 module.exports = router;
